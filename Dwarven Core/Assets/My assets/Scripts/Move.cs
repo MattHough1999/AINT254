5eb5dd0767266c3279ad47e516f69bd784f5ef7e
MattHough1999 <43546852+MattHough1999@users.noreply.github.com>
2019-10-03 12:42:53 +0100

    Added Basic movement
    Added Basic movement to allow to me move around the terain

--- Dwarven Core/Assets/My assets/Scripts/Move.cs
+++ Dwarven Core/Assets/My assets/Scripts/Move.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public Rigidbody rigidbody;
+    public Rigidbody player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.D)) player.velocity = new Vector3(20, 0, 0);
+        if (Input.GetKeyDown(KeyCode.A)) player.velocity = new Vector3(-20, 0, 0);
+        if (Input.GetKeyDown(KeyCode.W)) player.velocity = new Vector3(0, 0, 20);
+        if (Input.GetKeyDown(KeyCode.S)) player.velocity = new Vector3(0, 0, -20);
     }
 }
