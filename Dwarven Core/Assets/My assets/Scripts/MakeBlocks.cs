11def158443c93be939cdf4b240952d349e68770
MattHough1999 <matthough.uk@gmail.com>
2019-10-04 18:12:25 +0100

    Added block spawner
    added a fucntion to spawn a specified number of blocks

---
+++ Dwarven Core/Assets/My assets/Scripts/MakeBlocks.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeBlocks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject prefab;
+    GameObject[] prefabs;
+    public int worldSize = 5;
+    public int worldHeight = 5;
+    public int worldWidth = 5;
+    public Vector3 startPosition = new Vector3(0,0,0);
+    
+    private void Start()
+    {
+        SpawnBlocks();
+    }
+    public void SpawnBlocks() 
+    {
+        Vector3 CurrPosition = startPosition;
+        prefabs = new GameObject[worldSize];
+        int i = 0 ;
+        while (i < worldSize)
+        {
+            for (int x = 0; x < worldWidth; x++)
+            {
+                for (int z = 0; z < worldHeight; z++)
+                {
+                    if (i < worldSize)
+                    {
+                        GameObject block = Instantiate(prefab);
+                        block.transform.position = CurrPosition;
+                        i++;
+                    }
+                    CurrPosition.z++;
+                }
+                CurrPosition.x++;
+                CurrPosition.z = startPosition.z;
+            }
+            CurrPosition.x = startPosition.x;
+            CurrPosition.y++;
+
+        }
+
+    }
+}
--- Dwarven Core/Assets/My assets/Scripts/Move.cs
+++ Dwarven Core/Assets/My assets/Scripts/Move.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     public Rigidbody player;
+    public Vector3 maxVelocity = new Vector3(5,5,5);
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        while (Input.GetKeyDown(KeyCode.A)) player.velocity = player.velocity + new Vector3(20, 0, 0);
-        while (Input.GetKeyDown(KeyCode.A)) player.velocity = player.velocity + new Vector3(-20, 0, 0);
-        while (Input.GetKeyDown(KeyCode.W)) player.velocity = player.velocity + new Vector3(0, 0, 20);
-        while (Input.GetKeyDown(KeyCode.S)) player.velocity = player.velocity + new Vector3(0, 0, -20);
+        if (Input.GetKey(KeyCode.A) && player.velocity.x < maxVelocity.x) player.velocity = player.velocity + new Vector3(2, 0, 0);
+        if (Input.GetKey(KeyCode.D) && player.velocity.x > maxVelocity.x * -1) player.velocity = player.velocity + new Vector3(-2, 0, 0);
+        if (Input.GetKey(KeyCode.W) && player.velocity.z < maxVelocity.z) player.velocity = player.velocity + new Vector3(0, 0, 2);
+        if (Input.GetKey(KeyCode.S) && player.velocity.z > maxVelocity.z * -1) player.velocity = player.velocity + new Vector3(0, 0, -2);
     }
 }
