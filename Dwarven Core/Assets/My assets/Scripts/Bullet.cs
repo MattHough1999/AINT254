480a1b501940d6774282569aa7ca26af8572a84d
MattHough1999 <matthough.uk@gmail.com>
2019-10-07 01:22:26 +0100

    Added bullets
    added a gun and bullets to shoot

---
+++ Dwarven Core/Assets/My assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * 20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Dwarven Core/Assets/My assets/Scripts/Fire.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public GameObject Spawn;
+    public GameObject Prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1) == true) 
+        {
+            Instantiate(Prefab);
+            Prefab.transform.position = Spawn.transform.position;
+        }
+    }
+}
