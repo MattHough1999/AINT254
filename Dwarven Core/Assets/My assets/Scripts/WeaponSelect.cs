077fe67997be377c6ac1aa41415c484ca5a8be12
MattHough1999 <43546852+MattHough1999@users.noreply.github.com>
2019-10-31 15:54:29 +0000

    removed standard assets
    removed standard assests and removes obseletes

--- Dwarven Core/Assets/My assets/Scripts/WeaponSelect.cs
+++ Dwarven Core/Assets/My assets/Scripts/WeaponSelect.cs
@@ -5,43 +5,39 @@ using UnityEngine;
 public class WeaponSelect : MonoBehaviour
 {
     // Start is called before the first frame update
-    /// <summary>
-    /// GET RID OF ALL OF THIS CODE!!!! USE ONLY FOR THE DEMO!!!!!!!!!
-    /// </summary>
+    
     void Start()
     {
-        transform.GetChild(0).gameObject.active = true;
-        transform.GetChild(1).gameObject.active = false;
-    }
 
+        transform.GetChild(0).gameObject.SetActive(true);
+        transform.GetChild(1).gameObject.SetActive(false);
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E)) 
+        if (Input.GetKeyDown(KeyCode.E))
         {
             toggleWeapon();
         }
     }
-    /// <summary>
-    /// GET RID OF ALL OF THIS CODE!!!! USE ONLY FOR THE DEMO!!!!!!!!!
-    /// </summary>
-    void toggleWeapon() 
+
+
+   // [System.Obsolete]
+    void toggleWeapon()
     {
-        
-        if (transform.GetChild(0).gameObject.active == false)
+
+        if (transform.GetChild(0).gameObject.activeSelf == false)
         {
-            transform.GetChild(0).gameObject.active = true;
-            transform.GetChild(1).gameObject.active = false;
+            transform.GetChild(0).gameObject.SetActive(true) ;
+            transform.GetChild(1).gameObject.SetActive(false);
         }
 
         else
         {
-            transform.GetChild(1).gameObject.active = true;
-            transform.GetChild(0).gameObject.active = false;
+            transform.GetChild(1).gameObject.SetActive(true);
+            transform.GetChild(0).gameObject.SetActive(false);
         }
 
     }
-    /// <summary>
-    /// GET RID OF ALL OF THIS CODE!!!! USE ONLY FOR THE DEMO!!!!!!!!!
-    /// </summary>
 }
