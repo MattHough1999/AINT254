580a2a4788074805cd920b7dec137ef7469ea2a9
MattHough1999 <matthough.uk@gmail.com>
2019-11-11 22:48:14 +0000

    reverting
    At some point after this a bug occurred in my scripting that I couldn't pindown/fix to do with my colliders

--- Dwarven Core/Assets/My assets/Scripts/WeaponSelect.cs
+++ Dwarven Core/Assets/My assets/Scripts/WeaponSelect.cs
@@ -5,39 +5,43 @@ using UnityEngine;
 public class WeaponSelect : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+    /// <summary>
+    /// GET RID OF ALL OF THIS CODE!!!! USE ONLY FOR THE DEMO!!!!!!!!!
+    /// </summary>
     void Start()
     {
-
-        transform.GetChild(0).gameObject.SetActive(true);
-        transform.GetChild(1).gameObject.SetActive(false);
+        transform.GetChild(0).gameObject.active = true;
+        transform.GetChild(1).gameObject.active = false;
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E)) 
         {
             toggleWeapon();
         }
     }
-
-
-   // [System.Obsolete]
-    void toggleWeapon()
+    /// <summary>
+    /// GET RID OF ALL OF THIS CODE!!!! USE ONLY FOR THE DEMO!!!!!!!!!
+    /// </summary>
+    void toggleWeapon() 
     {
-
-        if (transform.GetChild(0).gameObject.activeSelf == false)
+        
+        if (transform.GetChild(0).gameObject.active == false)
         {
-            transform.GetChild(0).gameObject.SetActive(true) ;
-            transform.GetChild(1).gameObject.SetActive(false);
+            transform.GetChild(0).gameObject.active = true;
+            transform.GetChild(1).gameObject.active = false;
         }
 
         else
         {
-            transform.GetChild(1).gameObject.SetActive(true);
-            transform.GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(1).gameObject.active = true;
+            transform.GetChild(0).gameObject.active = false;
         }
 
     }
+    /// <summary>
+    /// GET RID OF ALL OF THIS CODE!!!! USE ONLY FOR THE DEMO!!!!!!!!!
+    /// </summary>
 }
